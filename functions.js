c24c93d51504e17f489ba72a596c659f840478f6
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-23 14:12:58 -0300

    Stricts equals

--- functions.js
+++ functions.js
@@ -2,6 +2,11 @@ const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
   checkValue: (x) => x,
+  createUser: () => {
+    const user = { firstName: 'Agustin' };
+    user['lastName'] = 'Ramunno';
+    return user;
+  },
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,11 @@
 const functions = require('./functions');
 
+// toBe
+test('Adds 2 + 2 to NOT equal 5', () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+
+// not toBe
 test('Adds 2 + 2 to NOT equal 5', () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
@@ -11,10 +17,20 @@ test('Adds 2 + 2 to NOT equal 5', () => {
 // toBeTruthy matches anything that an if statement treat as true
 // toBeFalsy matches anything that an if statement treat as false
 
+// toBeNull
 test('Should be null', () => {
   expect(functions.isNull()).toBeNull();
 });
 
+// toBeFalsy
 test('Should be falsy', () => {
   expect(functions.checkValue(null)).toBeFalsy();
+});
+
+// toBeFalsy
+test('User should be Agustin Ramunno object', () => {
+  expect(functions.createUser()).toStrictEqual({
+    firstName: 'Agustin',
+    lastName: 'Ramunno',
+  });
 });
