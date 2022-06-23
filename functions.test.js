5c3d4da03eaa2b242c6cd18ccb4a2a4815b273e6
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-23 14:47:14 -0300

    async await test

--- functions.test.js
+++ functions.test.js
@@ -53,10 +53,19 @@ test('Admin should be in usernames', () => {
   expect(usernames).toContain('admin');
 });
 
-// Working with async data
-test('User fetched name should be Leanne Graham', () => {
+// * Working with async data
+
+// Promise
+// test('User fetched name should be Leanne Graham', () => {
+//   expect.assertions(1);
+//   return functions.fetchUser().then((data) => {
+//     expect(data.name).toEqual('Leanne Graham');
+//   });
+// });
+
+// Async Await
+test('User fetched name should be Leanne Graham', async () => {
   expect.assertions(1);
-  return functions.fetchUser().then((data) => {
-    expect(data.name).toEqual('Leanne Graham');
-  });
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual('Leanne Graham');
 });
